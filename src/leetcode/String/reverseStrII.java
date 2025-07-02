54213410e96d964ed9876274bf59d4f158cdd1ae
XIEGUIHAN <2440035508@qq.com>
2025-07-02 17:16:54 +0800

    7.1公司mac提交

--- src/leetcode/String/reverseStrII.java
+++ src/leetcode/String/reverseStrII.java
@@ -1,11 +1,35 @@
 package leetcode.String;
 
 public class reverseStrII {
-    public String reverseStr(String s, int k) {
-        return null;
+    public static String reverseStrMethod(String s, int k) {
+        char[] charArray = s.toCharArray();
+        int length = charArray.length;
+        for (int i = 0; i < length; i=i+2*k) {
+            if (i+k <= length){
+                reverse(charArray, i, i+k-1);
+            }else {
+                reverse(charArray, i, length-1);
+            }
+        }
+
+        return String.valueOf(charArray);
+    }
+
+
+    public static void reverse( char[] chars, int left, int right ){
+        while (left < right){
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
     }
 
     public static void main(String[] args) {
-
+        String s = "abcdefg";
+        int k = 2;
+        String res = reverseStrMethod(s, k);
+        System.out.println(res);
     }
 }
